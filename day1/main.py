4315fa808e7a61fff44866ddfbba53dd34942657
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-02-07 23:52:20 -0500

    completed python implementation

--- day1/main.py
+++ day1/main.py
@@ -1,11 +1,24 @@
 """
 Find the elf with the most calories and return it given the text document input
 """
+import os
 
-# convert the text document into an array totaling the calories
-# figure out how to read the text document
-# how do i skip linse?
-# each elf is seperated by blank space
-# each object in the json is an elf
+dir_path = os.path.dirname(os.path.realpath(__file__))
 
-# go through the array and return the max calories
+with open(f"{dir_path}/input.txt") as plainInput:
+    lines = [line.strip() for line in plainInput.readlines()]
+
+l = 0
+maxCalories = 0
+
+while l < len(lines):
+    elfCalories = 0
+
+    while l + 1 != len(lines) and lines[l] != "":
+        elfCalories += int(lines[l])
+        l += 1
+
+    maxCalories = max(maxCalories, elfCalories)
+    l += 1
+
+print(f"max calories, {maxCalories}")
