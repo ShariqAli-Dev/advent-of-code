6b806bf6a3c39b84b7078c3f76d80cdf5026c202
ShariqAli-Dev <shariqali.dev@gmail.com>
2023-02-07 22:57:03 -0500

    first commit

---
+++ day1/main.py
@@ -0,0 +1,11 @@
+"""
+Find the elf with the most calories and return it given the text document input
+"""
+
+# convert the text document into an array totaling the calories
+# figure out how to read the text document
+# how do i skip linse?
+# each elf is seperated by blank space
+# each object in the json is an elf
+
+# go through the array and return the max calories
